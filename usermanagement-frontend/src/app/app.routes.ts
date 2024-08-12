b0b53562fe2e1d9f5f8e7bfd0cc875427d1d89ed
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-08-13 01:33:17 +0530

    Admin Dashboard

--- usermanagement-frontend/src/app/app.routes.ts
+++ usermanagement-frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UpdateuserComponent } from './updateuser/updateuser.component';
 import { usersGuard,adminGuard } from './users.guard';
+import { UserslistComponent } from './userlist/userlist.component';
 
 export const routes: Routes = [
 
@@ -11,7 +12,7 @@ export const routes: Routes = [
     {path: 'register', component: RegisterComponent},
     {path: 'profile', component: ProfileComponent},
     {path: 'update/:id', component: UpdateuserComponent},
-    // {path: 'users', component: UserslistComponent, canActivate:[adminGuard]},
+    {path: 'users', component: UserslistComponent, canActivate:[adminGuard]},
     {path: '**', component: LoginComponent},
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     
