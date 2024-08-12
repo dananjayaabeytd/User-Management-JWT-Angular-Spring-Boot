aa72edf6b7908c1adf6071b4875c1ab618863992
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-08-12 16:36:06 +0530

    first commit

---
+++ usermanagement/src/main/java/com/user/usermanagement/repository/UsersRepo.java
@@ -0,0 +1,12 @@
+package com.user.usermanagement.repository;
+
+
+import com.user.usermanagement.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers,Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
---
+++ usermanagement/src/main/java/com/user/usermanagement/service/JWTUtils.java
@@ -0,0 +1,22 @@
+package com.user.usermanagement.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Component
+public class JWTUtils {
+
+    private SecretKey Key;
+    private static final long EXPIRATION_TIME = 86400000;//24hours;
+
+
+    public JWTUtils(){
+        String secretString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+        byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+}
