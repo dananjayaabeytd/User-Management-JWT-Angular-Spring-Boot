0be9f88deee961f168f7695bf59b82c15677e04a
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-08-13 00:01:11 +0530

    Nav

--- usermanagement-frontend/src/app/app.routes.ts
+++ usermanagement-frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UpdateuserComponent } from './updateuser/updateuser.component';
+import { usersGuard,adminGuard } from './users.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'update/:id', component: UpdateuserComponent},
+    // {path: 'users', component: UserslistComponent, canActivate:[adminGuard]},
+    {path: '**', component: LoginComponent},
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    
+];
